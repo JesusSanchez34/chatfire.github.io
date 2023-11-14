446e476fdc7182b32de8a2b7dc8a7befef640859
JesusSanchez34 <139493968+JesusSanchez34@users.noreply.github.com>
2023-11-14 09:58:59 -0600

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyDljtAATxhQ3KolNWHU7CscYSuctV3hJqU",
+  authDomain: "chatfire-b9f23.firebaseapp.com",
+  projectId: "chatfire-b9f23",
+  storageBucket: "chatfire-b9f23.appspot.com",
+  messagingSenderId: "937472453462",
+  appId: "1:937472453462:web:dff2d44d30c864544dc4f7",
+  measurementId: "G-JYD2M4P0VE"
+    });
